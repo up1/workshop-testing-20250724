540e1bea7d145d26ba595f8f6d8b87b6650e8eab
up1 <somkiat.p@gmail.com>
2025-07-24 19:12:33 +0700

    Demo

--- web-ui/tests/example.spec.ts
+++ web-ui/tests/example.spec.ts
@@ -2,6 +2,8 @@ import { test, expect } from '@playwright/test';
 
 test('test', async ({ page }) => {
   await page.goto('https://seleniumbase.io/coffee/');
+  await page.locator('[data-test="Espresso"]').click();
+  await page.locator('[data-test="Espresso"]').click();
   const firstCoffee = page.getByText('Espresso $10.00espresso')
   await expect(firstCoffee).toHaveScreenshot();
 });
